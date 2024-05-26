5cf7d2a463622f9f088fd614828860aa7f9e8067
ZiadAlsaedi <ziad12399@gmail.com>
2024-05-26 19:10:01 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+//1
+let h1Color=document.getElementsByClassName("h1Class")[0].style.color = "blue";
+let h1BgColor=document.getElementsByClassName("h1Class")[0].style.backgroundColor = "black";
+//2
+let name1= document.getElementById("name")
+name1.setAttribute("class", "name");
+name1.style.backgroundColor="blue"
+name1.style.fontWeight="bold"
+name1.style.color="green"
+
+
+//3
+const image = document.getElementsByTagName('img')[0].src="https://training.epam.ua/static/news/255/self-study_03375353.png"
+console.log(image);
+//4
+let list=document.querySelector('ul').style.color="blue"
+let listborder=document.querySelector('ul').style.borderStyle="solid"
+let listW=document.querySelector('ul').style.display="inline-block"
+//5
+
