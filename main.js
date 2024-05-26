89b5c2d91070d26195cd4d708402f4edfb3fe06f
ZiadAlsaedi <ziad12399@gmail.com>
2024-05-26 19:56:01 +0300

    v3

--- main.js
+++ main.js
@@ -6,15 +6,32 @@ h1Class.style.backgroundColor = "black";
 let name1= document.getElementById("name")
 name1.style.fontWeight="bold"
 name1.setAttribute("class", "name");
-
-
-
 //3
 const image = document.getElementsByTagName('img')[0].src="https://training.epam.ua/static/news/255/self-study_03375353.png"
-console.log(image);
 //4
-let list=document.querySelector('ul').style.color="blue"
-let listborder=document.querySelector('ul').style.borderStyle="solid"
-let listW=document.querySelector('ul').style.display="inline-block"
+let list=document.querySelector('ul')
+let item= `<li>item1</li>
+<li>item2</li>
+<li>item3</li>
+`
+list.insertAdjacentHTML("afterbegin",item)
+list.style.border="2px solid black"
+list.style.color="blue"
+list.style.display="flex"
+list.style.justifyContent="space-between"
+
+
+
+
+//another way
+// for (let index = 0; index < 3; index++) {
+//     let item=document.createElement("li")
+//     item.innerText=`item ${index}`
+//     list.append(item)   
+// }
+
+
 //5
+
+
 
